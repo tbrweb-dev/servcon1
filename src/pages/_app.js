636321f930fdf1d8ca1c7c23a1e6ae926ca39949
TBRWEB - Desenvolvimento de Sites <tbrweb01@gmail.com>
2022-09-08 14:36:52 -0300

    SEO

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,6 +6,7 @@ import { initGA, logPageView } from 'analytics';
 */
 import 'assets/css/react-slick.css';
 import { InformacoesProvider } from 'hooks/useInformacoes';
+import Head from 'next/head';
 
 export default function CustomApp({ Component, pageProps }) {
   /** 
@@ -18,6 +19,30 @@ export default function CustomApp({ Component, pageProps }) {
 
   return (
     <InformacoesProvider>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no"
+        />
+        <title>Servcon Contabilidade</title>
+        <link rel="icon" href="assets/images/favicon.png" type="image/x-icon" />
+        <meta name="description" content="Prestar serviços contábeis e assessoria de qualidade, buscando atender as necessidades de nossos clientes e parceiros" />
+        <meta name="author" content="TBrWeb - Criação e Desenvolvimento de Sites" />
+
+        <meta property="og:title" content="Servcon Contabilidade" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://www.servcon1.com" />
+        <meta property="og:image" content="https://www.servcon1.com/assets/images/logo.png" />
+        <meta property="og:description" content="Prestar serviços contábeis e assessoria de qualidade, buscando atender as necessidades de nossos clientes e parceiros" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:creator" content="Servcon Contabilidade" />
+        <meta name="twitter:site" content="https://www.servcon1.com" />
+        <meta name="twitter:image" content="https://www.servcon1.com/assets/images/logo.png" />
+        <meta name="twitter:domain" content="servcon1.com" />
+        <meta name="twitter:title" content="Servcon Contabilidade" />
+        <meta name="twitter:description" content="Prestar serviços contábeis e assessoria de qualidade, buscando atender as necessidades de nossos clientes e parceiros" />
+      </Head>
       <Component {...pageProps} />
     </InformacoesProvider>
   );
